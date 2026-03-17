727d9971c642131bba17aecc4aaa4368ff93af56
Brice Kutuk <bricekutuk@live.fr>
2026-03-17 17:18:09 +0100

    change acceuil page and add the lucky wheel

--- roueDeLaChancePageDAccueil.js
+++ roueDeLaChancePageDAccueil.js
@@ -10,63 +10,6 @@ let vitesseRotation = 0;
 let enRotation = false;
 
 
-function dessinerRoue() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    const angleParOption = (Math.PI * 2) / options.length;
-    
-    for (let i = 0; i < options.length; i++) {
-        const angle = i * angleParOption;
-        
-        ctx.beginPath();
-        ctx.moveTo(canvas.width / 2, canvas.height / 2);
-        ctx.arc(canvas.width / 2, canvas.height / 2, 200, angle, angle + angleParOption);
-        ctx.fillStyle = couleurs[i];
-        ctx.fill();
-        
-        ctx.save();
-        ctx.translate(canvas.width / 2, canvas.height / 2);
-        ctx.rotate(angle + angleParOption / 2);
-        ctx.textAlign = 'right';
-        ctx.fillStyle = '#fff';
-        ctx.font = 'bold 18px Roboto';
-        ctx.fillText(options[i], 180, 0);
-        ctx.restore();
-    }
-}
-
-
-function animerRoue() {
-    if (vitesseRotation > 0.01) {
-        angleActuel += vitesseRotation;
-        vitesseRotation *= 0.99;
-        
-        ctx.save();
-        ctx.translate(canvas.width / 2, canvas.height / 2);
-        ctx.rotate(angleActuel);
-        ctx.translate(-canvas.width / 2, -canvas.height / 2);
-        dessinerRoue();
-        ctx.restore();
-        
-        requestAnimationFrame(animerRoue);
-    } else {
-        enRotation = false;
-        vitesseRotation = 0;
-        const optionGagnante = options[Math.floor(angleActuel / (Math.PI * 2 / options.length)) % options.length];
-        alert('Votre prochaine destination : ' + optionGagnante);
-    }
-}
-
-
-
-boutonTourner.addEventListener('click', () => {
-    if (!enRotation) {
-        enRotation = true;
-        vitesseRotation = Math.random() * 0.2 + 0.1;
-        animerRoue();
-    }
-});
-
 function dessinerIndicateur() {
     ctx.beginPath();
     ctx.moveTo(canvas.width / 2, 20);
@@ -76,5 +19,84 @@ function dessinerIndicateur() {
     ctx.fill();
 }
 
+// 🎡 ROUE 
+function dessinerRoue() {
+    const angleParOption = (Math.PI * 2) / options.length;
 
-dessinerRoue();
+    for (let i = 0; i < options.length; i++) {
+        const angle = i * angleParOption;
+
+        ctx.beginPath();
+        ctx.moveTo(canvas.width / 2, canvas.height / 2);
+        ctx.arc(canvas.width / 2, canvas.height / 2, 200, angle, angle + angleParOption);
+        ctx.fillStyle = couleurs[i];
+        ctx.fill();
+
+        
+        ctx.save();
+        ctx.translate(canvas.width / 2, canvas.height / 2);
+        ctx.rotate(angle + angleParOption / 2);
+        ctx.textAlign = 'center';
+        ctx.fillStyle = '#fff';
+        ctx.font = 'bold 18px Roboto';
+        ctx.fillText(options[i], 140, 10);
+        ctx.restore();
+    }
+}
+
+// 🎨 SCÈNE COMPLÈTE
+function dessinerScene() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // rotation de la roue
+    ctx.save();
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.rotate(angleActuel);
+    ctx.translate(-canvas.width / 2, -canvas.height / 2);
+
+    dessinerRoue();
+    ctx.restore();
+
+    
+    dessinerIndicateur();
+}
+
+// 🔄 ANIMATION
+function animerRoue() {
+    if (vitesseRotation > 0.001) {
+        angleActuel += vitesseRotation;
+        vitesseRotation *= 0.995;
+
+        dessinerScene();
+
+        requestAnimationFrame(animerRoue);
+    } else {
+        enRotation = false;
+        vitesseRotation = 0;
+
+        // 🎯 CALCUL EXACT DU GAGNANT
+const angleParOption = (Math.PI * 2) / options.length;
+
+    let angle = angleActuel % (Math.PI * 2);
+    angle = (Math.PI * 2) - angle;
+    angle += Math.PI / 2 + Math.PI;
+
+const index = Math.floor(angle / angleParOption) % options.length;
+const optionGagnante = options[index];
+
+        alert('Votre prochaine destination : ' + optionGagnante);
+    }
+}
+
+// 🎯 BOUTON
+boutonTourner.addEventListener('click', () => {
+    if (!enRotation) {
+        enRotation = true;
+        vitesseRotation = Math.random() * 0.2 + 0.1;
+        animerRoue();
+    }
+});
+
+dessinerScene();
+
+// FIN DE SECTION //
